6bfdaa5b964f8ceada3f15eacbdc70094980b023
Jesse Haubrich <state_of_the_geos@faa.none>
2018-06-05 15:28:45 +0000

    fresh .emacs.d for 26.1

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (beacon highlight-symbol dumb-jump yasnippet-snippets cmake-ide flycheck-irony flycheck-rtags company-irony-c-headers company-irony-headers company-irony irony irony-mode helm-rtags rtags-helm company-rtags pkgbuild-mode clojure-mode golden-ratio helm helm-c-yasnippet elpy org-beautify-theme org-beautify noctilux-theme dracula-theme smartparens paredit-everywhere clj-refactor flycheck-clojure paredit 4clojure cider systemd pyvenv ac-cider ein sml-mode mu4e-maildirs-extension csv-mode zzz-to-char zop-to-char helm-themes helm-descbinds undo-tree jedi auto-yasnippet multi-term xkcd web-mode spaceline sos smex rcirc-notify rainbow-mode rainbow-identifiers pkg-info page-break-lines org-bullets multiple-cursors mingus markdown-mode magit lua-mode jinja2-mode ido-vertical-mode ido-hacks ibuffer-vc helm-swoop guide-key git-timemachine git-gutter-fringe gist flx f expand-region edit-server dockerfile-mode darktooth-theme counsel charmap bug-hunter bbcode-mode avy auto-complete ag)))
+    (yaml-mode swiper yasnippet beacon highlight-symbol dumb-jump yasnippet-snippets cmake-ide flycheck-irony flycheck-rtags company-irony-c-headers company-irony-headers company-irony irony irony-mode helm-rtags rtags-helm company-rtags pkgbuild-mode clojure-mode golden-ratio helm helm-c-yasnippet elpy org-beautify-theme org-beautify noctilux-theme dracula-theme smartparens paredit-everywhere clj-refactor flycheck-clojure paredit 4clojure cider systemd pyvenv ac-cider ein sml-mode mu4e-maildirs-extension csv-mode zzz-to-char zop-to-char helm-themes helm-descbinds undo-tree jedi auto-yasnippet multi-term xkcd web-mode spaceline sos smex rcirc-notify rainbow-mode rainbow-identifiers pkg-info page-break-lines org-bullets multiple-cursors mingus markdown-mode magit lua-mode jinja2-mode ido-vertical-mode ido-hacks ibuffer-vc helm-swoop guide-key git-timemachine git-gutter-fringe gist flx f expand-region edit-server dockerfile-mode darktooth-theme counsel charmap bug-hunter bbcode-mode avy auto-complete ag)))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
@@ -92,7 +92,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#202020" :foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "ADBO" :family "Source Code Pro"))))
- '(flymake-errline ((t (:inherit nil :underline nil))))
+ '(flymake-error ((t (:inherit nil :underline nil))))
  '(fringe ((t (:background "#202020" :foreground "#5f5f5f"))))
  '(highlight-symbol-face ((t (:background "dodger blue"))))
  '(mu4e-header-highlight-face ((t (:background "#b6b7c1"))))
